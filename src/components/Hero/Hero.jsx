5856d00d9841cf27828b3c6a2a776020cea07251
mangihut <martua2201@gmail.com>
2026-07-17 23:37:38 +0700

    continue

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,22 +1,61 @@
-import "./Hero.css";
-import Button from "../Button/Button";
+import hero from "../../assets/images/hero.jpg";
+import { Link } from "react-router-dom";
+import { FaCoffee } from "react-icons/fa";
 
 function Hero() {
-    return(
-         <section className="hero" id="home">
-      <div className="hero-content">
-        <h2 className="text-3xl font-bold text-amber-700">Selamat Datang di Cafe Delight</h2>
+  return (
+    <section className="bg-amber-50">
+      <div className="max-w-7xl mx-auto px-6 py-16 md:py-18">
 
-        <p>
-          Tempat yang sempurna untuk menikmati kopi dan makanan lezat
-        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-12">
 
-        <Button onClick={() => console.log("Button diklik")}>
-            Lihat Menu
-        </Button>
+          <div className="text-center md:text-left justify-center md:justify-start">
+
+            <span className="flex items-center justify-center w-52 mx-auto bg-orange-200 text-orange-700 px-4 py-2 gap-3 rounded-full text-sm font-semibold mb-6">
+              <FaCoffee /> Premium Coffee
+            </span>
+
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tight max-w-xl text-gray-900">
+              Fresh Coffee
+              <br />
+              <span className="text-amber-700">
+                Everyday
+              </span>
+            </h1>
+
+            <p className="mt-6 text-gray-600 text-lg leading-relaxed max-w-lg">
+              Nikmati kopi premium dengan cita rasa terbaik
+              untuk menemani setiap momenmu.
+            </p>
+
+            <div className="flex items-center justify-center flex-wrap gap-4 mt-8">
+              
+              <Link to="/menu"
+                    className="bg-orange-800 text-white px-6 py-3 rounded-lg hover:bg-amber-700 transition">
+                    Explore Menu
+              </Link>
+
+              <Link to="/contact"
+                    className="border-2 border-orange-800 text-orange-800 px-6 py-3 rounded-lg hover:bg-orange-800 hover:text-white transition">
+                    Reserve Table
+              </Link>
+
+            </div>
+
+          </div>
+
+          {/* Right Image */}
+          <div className="flex justify-center">
+            <img src={hero} alt="Fresh Coffee"
+              className="w-full max-w-md rounded-3xl shadow-2xl object-cover"
+            />
+          </div>
+
+        </div>
+
       </div>
     </section>
-    );
+  );
 }
 
 export default Hero;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,47 @@
-import "./Navbar.css";
+import logo from "../../assets/images/logo.png";
+import { NavLink } from "react-router-dom";
+
+
+// menu list 
+const menus = [ {name: "Home",path: "/"},
+                {name: "About",path: "/about"},
+                {name: "Menu",path: "/menu"},
+                {name: "Contact",path: "/contact"},
+              ]
+
 function Navbar () {
     return (
-        <div className="navbar">
-                < div className="">
-                <img src="Logo.png"></img></div>
-                <h1 className="navbar-brand">Cafe Delight </h1>
-                <nav className="navbar-nav" id="navbar-nav">
-                    <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#menu">Menu</a></li>
-                    <li><a href="#contact">Contact Us</a></li>
-                    </ul>
-                </nav>
-            </div>
+       <nav className="flex justify-between items-center max-w-7xl mx-auto py-4 px-5 bg-amber-100 ">
+        {/* Logo */}
+        <div  className="flex gap-3"> 
+            <img src={logo} alt="Cafe Delight Logo" className="w-10 h-10 rounded-full" />
+            <span className="text-2xl font-semibold uppercase tracking-wide text-amber-700">Cafe Delight</span>
+        </div>
+     
+        {/* import list menu */}
+       <ul className="flex gap-6">
+            {menus.map((menu) => (
+                <li key={menu.name}>
+                <NavLink
+                    to={menu.path}
+                    className={({ isActive }) =>
+                    isActive
+                        ? "text-amber-700 font-semibold border-b-2 border-amber-700"
+                        : "hover:text-amber-700 transition"
+                    }
+                >
+                    {menu.name}
+                </NavLink>
+                </li>
+            ))}
+        </ul>
+       
+        <button className="bg-orange-800 text-white px-5 py-2 rounded-lg
+                           hover:bg-amber-700 hover:scale-105
+                              transition-all duration-300">
+            Reserve Table
+        </button>
+       </nav>
     );
 }
 
